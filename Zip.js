2ebadf87d41bf706ed7b01fbd9cd74ba2d12cd36
Leo G. Dion <leogdion@brightdigit.com>
2013-04-24 13:16:07 +0000

    fixed zip

--- Zip.js
+++ Zip.js
@@ -5,81 +5,22 @@ var Zip = (function () {
     async = require('async'),
     fs = require('fs'),
     path = require('path');
-
-/*
-  var instance = function (directory) {
-    this.directory = directory;
   };
+  
 
-  instance.prototype = {
-    begin : function (cb) {
-      this.cb = cb;
-      this.zipfile = path.join(os.tmpDir(), randomizer() + ".zip");
-      this.zip = zip();
-      this.addFiles([this.directory], this.finish.bind(this, cb));
-    },
-    addFiles : function (files, cb) {
-      async.eachSeries(files, this.drill.bind(this), cb);
-    },
-    drill : function (file, cb) {
-      fs.stat(file, this.onstat.bind(this, file, cb));
-    },
-    onstat : function (file, cb, error, stats) {
-      if (error) {
-        cb(error);
-        return;
-      }
-      if (stats.isDirectory()) {        
-        fs.readdir(file, this.onreaddir.bind(this, cb, file));
-      } else {
-        fs.readFile(file, this.onreadfile.bind(this, cb, file));
-      }
-    },
-    onreaddir : function (cb, dir, error, files) {
-      if (error) {
-        cb(error);
-        return;
-      }
-      async.map(files, this.resolvepath.bind(this, dir), this.pathresolved.bind(this, cb));
-    },
-    onreadfile : function (cb, file, error, data) {
-      if (error) {
-        cb(error);
-        return;
-      }
-      this.zip.file(path.relative(this.directory, file), data.toString());
-      cb();
-    },
-    resolvepath : function (dir, file, cb) {
-      cb(undefined, path.resolve(dir, file));
-    },
-    pathresolved : function (cb, error, files) {
-      if (error) {
-        cb(error);
-        return;
-      }
-      this.addFiles(files, cb);      
-    },
-    finish : function (cb, error) {
-      if (error) {
-        cb(error);
-        return;
-      }
-      console.log(this.directory + ": writing file...");
-      fs.writeFile(this.zipfile,  this.zip.generate({base64:false,compression:'DEFLATE'}), 'binary', this.end.bind(this, cb));
-    },
-    end : function (cb, error) {
-      cb(error, this.zipfile);
-    }
-  };
-*/
-  var my = function (directory, cb) {
+ var my = function (directory, cb) {
     var zipfile = path.join(os.tmpDir(), randomizer() + ".zip");
-    var zip = spawn('zip', ['-R', zipfile, "*"], { cwd : directory });
-    zip.stderr.on('data', function (data) {
-      cb(error);
+    console.log('zipping ' + directory + ' to ' + zipfile);
+    var zipps = spawn('zip', ['-R', zipfile, "*"], { cwd : directory });
+    zipps.stdout.on('data', function (data) {
+     // console.log(data.toString());
     });
-    zip.on('close', function (code) {
+    zipps.stderr.on('data', function (data) {
+      console.log(data.toString());
+      cb(data);
+    });
+    zipps.on('close', function (code) {
+      console.log('completed ' + zipfile);
       cb(code || undefined, zipfile);
     });
   };
